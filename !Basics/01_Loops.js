beb56ac0d98e80491f4019916e022acbce9df156
Saddam Hussein <work.saddam786@gmail.com>
2025-07-23 23:53:13 +0530

    add isPrime

--- !Basics/01_Loops.js
+++ !Basics/01_Loops.js
@@ -21,3 +21,20 @@ function printFibonacciSeries(n) {
   }
 }
 // printFibonacciSeries(5)          //0,1,1,2,3
+
+// Check Prime or not:-------------------------------------------
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+// console.log(isPrime(7))      //true
+// console.log(isPrime(10))     //false
