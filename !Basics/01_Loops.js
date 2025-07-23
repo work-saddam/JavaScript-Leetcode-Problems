950006f0b35aaa721573dea6c932b7bca37eae68
Saddam Hussein <work.saddam786@gmail.com>
2025-07-23 23:52:34 +0530

    add fibonacii

--- !Basics/01_Loops.js
+++ !Basics/01_Loops.js
@@ -6,3 +6,18 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 // console.log(factorial(5));   //120
+
+// Fibonacci Series:--------------------------------------
+function printFibonacciSeries(n) {
+  function fibonacci(num) {
+    if (num <= 1) {
+      return num;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+
+  for (let i = 0; i < n; i++) {
+    console.log(fibonacci(i));
+  }
+}
+// printFibonacciSeries(5)          //0,1,1,2,3
