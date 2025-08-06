9310d9b1a808fe855877a2a16bd14ed8ddebc244
Saddam Hussein <work.saddam786@gmail.com>
2025-08-06 16:37:11 +0530

    1st non repeeating char in string (itterative way)

--- !Basics/02_String.js
+++ !Basics/02_String.js
@@ -1,39 +1,42 @@
-//  Q) write a program to convert first letter capital of every word.
+//  Q) write a program to convert first letter capital of every word.-------------------------------
 //  using built-in function
 function capital(str) {
-  str = str.trim()
+  str = str.trim();
   const word = str.split(" ");
   for (let i = 0; i < word.length; i++) {
-    if(word[i].length > 0){
-    word[i] = word[i][0].toUpperCase() + word[i].substring(1);
+    if (word[i].length > 0) {
+      word[i] = word[i][0].toUpperCase() + word[i].substring(1);
     }
   }
   res = word.join(" ");
   return res;
 }
-// console.log(capital("   with space  "));   
+// console.log(capital("   with space  "));
 // console.log(capital("without space"));
 
-//  Q) write a program to convert first letter capital of every word.
+//  Q) write a program to convert first letter capital of every word.---------------------------------
 // without using built-in function
 
 function capitalFun(str) {
   let newStr = "";
   for (let i = 0; i < str.length; i++) {
-    if(str[i]==" " && (str[i-1]===" "||str[i+1]===" ")){
+    if (str[i] == " " && (str[i - 1] === " " || str[i + 1] === " ")) {
       continue;
-    }else if(str[i]!==" " && (str[i-1]===" " || str[i-1]===undefined)){
-      newStr= newStr + str[i].toUpperCase()
-    }else{
-      newStr=newStr + str[i]
+    } else if (
+      str[i] !== " " &&
+      (str[i - 1] === " " || str[i - 1] === undefined)
+    ) {
+      newStr = newStr + str[i].toUpperCase();
+    } else {
+      newStr = newStr + str[i];
     }
   }
-  return newStr
+  return newStr;
 }
 // console.log(capitalFun("   with space  "));
 // console.log(capitalFun("without space"));
 
-// reverse String
+// reverse String -------------------------------------------------------------------------------
 function reverseString(str) {
   let reversed = "";
   for (let i = str.length - 1; i >= 0; i--) {
@@ -49,3 +52,21 @@ function revString(str) {
   return res;
 }
 // console.log(revString("hello"));  // "olleh"
+
+// first non repeating character in string --------------------------------------------------------
+function nonRep(str) {
+  for (let i = 0; i < str.length; i++) {
+    let isPresent = false;
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        isPresent = true;
+        break;
+      }
+    }
+    if (!isPresent) {
+      return str[i];
+    }
+  }
+  return -1;
+}
+console.log(nonRep("asdasf"));
