427581fef527dd85c735185284fe279c4e0ca023
Saddam Hussein <work.saddam786@gmail.com>
2025-08-07 16:10:09 +0530

    second max & min number in array

--- !Basics/03_Array.js
+++ !Basics/03_Array.js
@@ -9,3 +9,32 @@ function revArray(arr) {
   return arr;
 }
 // console.log(revArray([2, 4, 7, 3, 4]));    // [4,3,7,4,2]
+
+// Find Second Maximum and Minimum number in array:-----------------------------------------
+function secondMaxMin(arr) {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+  let min = Infinity;
+  let secondMin = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    //For max
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+
+    //For min
+    if (arr[i] < min) {
+      secondMin = min;
+      min = arr[i];
+    } else if (arr[i] > min && arr[i] < secondMin) {
+      secondMin = arr[i];
+    }
+  }
+  console.log("secondMax: ", secondMax);
+  console.log("secondMin: ", secondMin);
+}
+// secondMaxMin([2, 4, 3, 7, 5, 4, 8]);    // secondMax: 7 || secondMin: 3
