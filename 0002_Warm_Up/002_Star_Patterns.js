8baeb64668e52486500f9d45aa30ba24f415d1d3
Saddam Hussein <work.saddam786@gmail.com>
2026-05-08 07:54:31 +0530

    add function to print binary patterns

--- 0002_Warm_Up/002_Star_Patterns.js
+++ 0002_Warm_Up/002_Star_Patterns.js
@@ -84,3 +84,27 @@ function pattern3(n) {
   }
 }
 // pattern3(5);
+
+/* Pattern4:
+------------
+1
+10
+101
+1010
+10101
+------------
+*/
+
+// Solution 4:-----------------------------------------------------------------------
+function pattern4(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+
+    for (let j = 1; j <= i; j++) {
+      if (j % 2 === 0) row += 0;
+      else row += 1;
+    }
+    console.log(row);
+  }
+}
+// pattern4(5);
